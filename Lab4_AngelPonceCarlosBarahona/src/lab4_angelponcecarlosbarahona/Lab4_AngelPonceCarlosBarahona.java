f729f9492ed28afd85b958ceb57d5b3a04e2dd9a
CJBarahona <cbarahona@t-developing.com>
2021-02-12 13:47:30 -0600

    Proyecto

---
+++ Lab4_AngelPonceCarlosBarahona/src/lab4_angelponcecarlosbarahona/Lab4_AngelPonceCarlosBarahona.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4_angelponcecarlosbarahona;
+
+/**
+ *
+ * @author Admin
+ */
+public class Lab4_AngelPonceCarlosBarahona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAA");
+        // TODO code application logic here
+    }
+    
+}
